7816824291ecd11a64a3d8002360a5cadf257777
Sohnyzhy <82497682+Sohnyzhy@users.noreply.github.com>
2022-02-23 13:11:36 +0100

    added first product

--- jsmodul/fetch.js
+++ jsmodul/fetch.js
@@ -7,6 +7,19 @@
   .then((data) => {
     console.log("FÅR UT DETTA", data);
 
+
+        document.querySelector(".p-name").innerText =  data.products[0].name;
+        document.querySelector(".price").innerText =  data.products[0].price;
+        document.querySelector(".size").innerText =  data.products[0].quantity;
+        document.querySelector(".img").src = data.products[0].photo     
+
+
+
+
+        console.log(data.products[0].name)
+
+       
+
   
     
   });
