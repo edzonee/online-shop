4bb32c1af06f65112c258181ebc06d96b48fcd24
edzonee <edzone1996@gmail.com>
2022-02-25 14:51:34 +0100

    fixed append.js

--- jsmodul/append.js
+++ jsmodul/append.js
@@ -15,8 +15,37 @@ export async function displayIt() {
         myStringArray[i].photo
       );
       myProducts.push(prod);
-       
+
+      const productsCard = document.createElement("div");
+      productsCard.classList.add("Products");
+      const cardWrap = document.createElement("div");
+      cardWrap.classList.add("card");
+
+      document.body.appendChild(cardWrap);
+
+      const price = document.createElement("div");
+      price.classList.add("price");
+      price.innerText = "Price: " + myStringArray[i].price + " kr";
+      const descrip = document.createElement("h4");
+      descrip.classList.add("description");
+      descrip.innerText = myStringArray[i].name;
+
+      const quantity = document.createElement("div");
+      quantity.innerText = "Quantity: " + myStringArray[i].quantity;
+
+      const button = document.createElement("button");
+      button.innerText = "Buy";
+
+      const photo = document.createElement("img");
+      photo.src = myStringArray[i].photo;
+
+      productsCard.appendChild(descrip);
+      cardWrap.appendChild(button);
+      cardWrap.append(productsCard);
+      productsCard.appendChild(photo);
+      productsCard.appendChild(price);
+      productsCard.appendChild(quantity);
     }
-    console.log(myProducts[0].photo)
+    console.log(myProducts);
   });
 }
